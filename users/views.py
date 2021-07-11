9669dcdaab89a7a5b734ce69eaa21c55f7b5a0a7
jtnavd <81948549+jtnavd@users.noreply.github.com>
2021-07-11 11:14:33 +0300

    main page

--- users/views.py
+++ users/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+
+messages = [
+    {
+        'sender' : 'John Doe',
+        'title' : 'Inquiry 1',
+        'content' : 'Lorem Ipsum',
+        'sender' : '1 jan 2002',
+    },
+    {
+        'sender' : 'Mary Jane',
+        'title' : 'Inquiry 2',
+        'content' : 'Lorem Ipsum',
+        'sender' : '1 jan 2004',
+    }
+]
+
+def base(request):
+    context = {
+        # 'sender' : sender,
+        # 'reciever' : reciever,
+        'messages' : messages,
+        # 'created_at' : created_at,
+    }
+    return render(request,'base.html', context)
 
 def register(request):
     form = UserCreationForm()
-    return render(request, 'partials/register.html', {'form': form})
+    return render(request, 'partials/_register.html')
