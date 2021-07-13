5a627d9e290e14776970e0220d7e2bed30225019
jtnavd <81948549+jtnavd@users.noreply.github.com>
2021-07-13 17:17:01 +0300

    readme + disable html pk

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', PostListView.as_view(), name='base-page'),
     path('message/new/', PostCreateView.as_view(), name='message-create'),
-    path('message/<pk>/', PostDetailView.as_view(), name='message-detail'),
-    path('message/<pk>/delete', PostDeleteView.as_view(), name='message-delete'), 
+    path('message/', PostDetailView.as_view(), name='message-detail'),
+    path('message/delete', PostDeleteView.as_view(), name='message-delete'), 
     path('message/new/', PostCreateView.as_view(), name='message-create'),
 ]
